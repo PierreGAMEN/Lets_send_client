a43a78a1c3458739b3282553fa2273fc1e134a14
Pierre GAMEN <pierre.gamen@gmail.com>
2024-09-12 18:39:32 +0200

    update Menu page

--- src/component/admin/MenuPage.jsx
+++ src/component/admin/MenuPage.jsx
@@ -1,13 +1,18 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
 import FormProduct from "./formProduct";
 import UpdateProduct from "./updateProduct";
 import OrderDisplay from "../waiter/waiter";
 import CreateUser from "./createUser";
-import UpdateUser from "./updateUser";
+import UpdateUser from "./UpdateUser/UpdateUser";
 import NavAdmin from "./navAdmin/navAdmin";
 import TablesWithProducts from "./tableWithProduct";
+import UpdateTable from "./Tables/updateTable";
+import AdminTable from "./Tables";
 
 const MenuPage = ({ user, openForm, handleForm }) => {
+  const [showMenuAdmin, setShowMenuAdmin] = useState(false); // Pour afficher/masquer "Administrer la carte"
+
   return (
     <section className="admin">
       {openForm.initMenu && (
@@ -15,12 +20,28 @@ const MenuPage = ({ user, openForm, handleForm }) => {
           {/* Affichage des boutons d'action pour les utilisateurs admin */}
           {user.right === "admin" && (
             <>
-              <button onClick={() => handleForm("createProduct")}>
-                Ajouter un produit à la carte
-              </button>
-              <button onClick={() => handleForm("updateProduct")}>
-                Modifier la carte
-              </button>
+              {/* Bouton pour administrer la carte */}
+              {!showMenuAdmin && <button onClick={() => setShowMenuAdmin(!showMenuAdmin)}>
+                Administrer la carte
+              </button>}
+              {showMenuAdmin && (
+                <>
+                  <button onClick={() => handleForm("createProduct")}>
+                    Ajouter un produit à la carte
+                  </button>
+                  <button onClick={() => handleForm("updateProduct")}>
+                    Modifier la carte
+                  </button>
+                  <button onClick={() => setShowMenuAdmin(!showMenuAdmin)}>
+                    Retour
+                  </button>
+                </>
+              )}
+
+              {/* Bouton pour administrer les tables */}
+              {!showMenuAdmin && <button onClick={() => handleForm("createTable")}>
+                Administrer les tables
+              </button>}
             </>
           )}
         </div>
@@ -32,13 +53,15 @@ const MenuPage = ({ user, openForm, handleForm }) => {
         {openForm.updateProduct && (
           <UpdateProduct company_id={user.company_id} />
         )}
-        {openForm.getOrders && <OrderDisplay company_id={user.company_id} />}
+        {openForm.createTable && <AdminTable company_id={user.company_id} />}
+        {openForm.updateTable && <UpdateTable company_id={user.company_id} />}
         {openForm.tableOrder && <TablesWithProducts company_id={user.company_id} />}
+        {openForm.getOrders && <OrderDisplay company_id={user.company_id} />}
         {openForm.createUser && <CreateUser company_id={user.company_id} />}
         {openForm.updateUser && (
           <UpdateUser
             company_id={user.company_id}
-            user_id={user.id}
+            user_id={user.user_id}
             username={user.username}
             role={user.right}
           />
