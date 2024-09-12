9b3be8e4c4b30dc1f4e8d1970c2fe0a94fab305e
Pierre GAMEN <pierre.gamen@gmail.com>
2024-09-12 18:32:43 +0200

    fix: path route on main

--- src/main.jsx
+++ src/main.jsx
@@ -3,27 +3,30 @@ import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Client from "./component/client";
 import Admin from "./component/admin/admin";
-import OrderDisplay from "./component/waiter/waiter";
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import App from "./App";
 
 
 const router = createBrowserRouter([
   {
-    path: "/client/:id_company/:id_table",
-    element: <Client />,
-  },
-  {
     path: "/",
-    element: <Admin />,
-  },
-  {
-    path: "/waiter",
-    element: <OrderDisplay />,
-  },
+    element: <App />,
+    children: [
+      { 
+        index: true,
+        element: <Admin />,
+      },
+      {
+        path: "/client/:id_company/:id_table",
+        element: <Client />,
+      },
+    ]
+  }
+  
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
