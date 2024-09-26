4b032a4721a8372c580ff396b0435179985088f8
Pierre GAMEN <pierre.gamen@gmail.com>
2024-09-26 15:43:10 +0200

    feat: add manifest to transform app on PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Lets send",
+        short_name: "LS",
+        description: "Application pour aider les bars et les restaurateurs à gagner en productivité.",
+        theme_color: "#ffffff",
+        start_url: "/",
+        display: "standalone",
+      },
+    }),
+  ],
+});
