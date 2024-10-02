e7b7b1bdc5bfd919bbf342991923d6c8cfc8ed19
Pierre GAMEN <pierre.gamen@gmail.com>
2024-10-02 10:39:31 +0200

    create netlify config for pwa

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      manifest: {
-        name: "Lets send",
-        short_name: "LS",
-        description: "Application pour aider les bars et les restaurateurs à gagner en productivité.",
-        theme_color: "#ffffff",
-        start_url: "/",
-        display: "standalone",
-      },
-    }),
-  ],
+  plugins: [react()],
 });
