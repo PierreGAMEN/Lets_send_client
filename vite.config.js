59c5ef2112c89c8753e1fb79572b3e667bcd0de2
Pierre GAMEN <pierre.gamen@gmail.com>
2024-10-02 11:00:28 +0200

    add PWA manif

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: 'Lets_send',
+        short_name: 'LS',
+        start_url: '/',
+        display: 'fullscreen', // Mode plein écran
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'LS.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'LS.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      // Autres options de configuration, si nécessaire
+    })
+  ],
 });
